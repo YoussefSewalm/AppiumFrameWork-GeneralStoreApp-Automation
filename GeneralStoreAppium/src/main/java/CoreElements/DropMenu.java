5744dc74ffb09263fbfde3903d0e30b69d727a81
YoussefSewalm <eng.yousefsewalem@gmail.com>
2024-12-08 06:41:47 +0200

    Update DropMenu.java

--- GeneralStoreAppium/src/main/java/CoreElements/DropMenu.java
+++ GeneralStoreAppium/src/main/java/CoreElements/DropMenu.java
@@ -10,8 +10,7 @@ public class DropMenu {
 	public static AndroidDriver driver;
 	By elementLocator;
 	
-	public static void SetDriver(AndroidDriver driver1) //3mllto set driver lwa7do brdo 3shan dh msh 3aml extend ll Elements 
-	//f lazem yb2a m3ahom nafs l driver 2ly et3ml fl initiate
+	public static void SetDriver(AndroidDriver driver1) 
 	{
 		driver=driver1;
 	}
@@ -24,7 +23,6 @@ public class DropMenu {
 	{
 		driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true).instance(0))" +
 		        ".scrollIntoView(new UiSelector().text(\"" + Country + "\").instance(0))")).click(); 
-		//AHAM 7AGA l INSTANCE 0 DEH 3SHAN DEH 2ly btozbot l scroll sa7
 	}
 	public void Click()
 	{
