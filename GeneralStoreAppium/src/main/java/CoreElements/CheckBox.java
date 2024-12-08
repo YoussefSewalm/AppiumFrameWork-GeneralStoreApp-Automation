c5c39cf7b742a1d5d2da0b310803c461a3f33d3e
YoussefSewalm <eng.yousefsewalem@gmail.com>
2024-12-08 06:41:31 +0200

    Update CheckBox.java

--- GeneralStoreAppium/src/main/java/CoreElements/CheckBox.java
+++ GeneralStoreAppium/src/main/java/CoreElements/CheckBox.java
@@ -9,8 +9,7 @@ public class CheckBox {
 	public static AndroidDriver driver;
     By elementLocator;
 	
-	public static void SetDriver(AndroidDriver driver1) //3mllto set driver lwa7do brdo 3shan dh msh 3aml extend ll Elements 
-	//f lazem yb2a m3ahom nafs l driver 2ly et3ml fl initiate
+	public static void SetDriver(AndroidDriver driver1)
 	{
 		driver=driver1;
 	}
