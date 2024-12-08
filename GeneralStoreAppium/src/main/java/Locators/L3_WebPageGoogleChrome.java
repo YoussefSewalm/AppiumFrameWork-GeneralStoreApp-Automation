f61e75d152b57e31971cb079c296617264519e05
YoussefSewalm <eng.yousefsewalem@gmail.com>
2024-12-08 06:46:13 +0200

    Update L3_WebPageGoogleChrome.java

--- GeneralStoreAppium/src/main/java/Locators/L3_WebPageGoogleChrome.java
+++ GeneralStoreAppium/src/main/java/Locators/L3_WebPageGoogleChrome.java
@@ -2,9 +2,7 @@ package Locators;
 
 import org.openqa.selenium.By;
 
-public class L3_WebPageGoogleChrome {
-//tb3an ana hena 7awelt Web ya3ny kol locators httgab mn l Web (Inspect Element Selenium 3ady bara 3n appium)
-	
+public class L3_WebPageGoogleChrome {	
 	//FormField
 	public static By SearchFormField=By.xpath("//textarea[@name=\"q\"]");
 	
